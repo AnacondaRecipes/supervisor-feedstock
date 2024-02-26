ba14318548e045b12075ecb31df1fc2b43de8029
Steven <scrass@anaconda.com>
2024-02-26 16:07:17 -0700

    update build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,7 +18,7 @@ function mkdir_touch()
     touch $dir/.mkdir
 }
 
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
+$PYTHON -m pip install --single-version-externally-managed --record record.txt --no-deps --no-build-isolation
 
 mkdir_touch $PREFIX/etc/supervisord/conf.d
 mkdir_touch $PREFIX/etc/supervisord/startup
